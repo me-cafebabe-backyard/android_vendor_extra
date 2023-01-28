e4857062aa4ddb04ca91f52a0a45ca78ff74b793
me-cafebabe <me.cafebabe@gmail.com>
2023-01-29 01:52:10 +0800

    extra: go: Re-enable boot image profile by default

--- product/go.mk
+++ product/go.mk
@@ -30,8 +30,10 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
-#PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-#PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+ifneq ($(EXTRA_DISABLE_BOOT_IMAGE_PROFILE),true)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+endif
 
 # Do not generate libartd.
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
